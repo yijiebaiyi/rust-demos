35349c95d7b8e01679c07a8c087b14d86b191279
tuojinxin <15998186418@163.com>
2024-02-23 16:23:38 +0800

    hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+D:\project\demo\rust-demos\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+D:\project\demo\rust-demos\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\project\demo\rust-demos\hello_cargo\target\debug\hello_cargo.exe: D:\project\demo\rust-demos\hello_cargo\src\main.rs
